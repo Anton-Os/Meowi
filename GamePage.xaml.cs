1789da6717448683ac3b8eb65b4fcaaa77c9798f
Anton Ostrovskiy <ao.omega2000@gmail.com>
2025-08-24 21:08:13 -0500

    Meowi Project

---
+++ GamePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Media;
+
+// using Plugin.Maui.Audio;
+
+namespace Meowi
+{
+    public partial class GamePage : ContentPage
+    {
+
+        public GamePage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnImageButtonClicked(object? sender, EventArgs e){
+            Console.WriteLine("Meow Pressed");
+            // TODO: Play cat noise
+        }
+
+        // private SoundPlayer player = new SoundPlayer();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Meowi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SetupGame();
+        }
+
+
+        private void SetupGame(){
+            Console.WriteLine("Setting up game");
+        }
+
+        async private void OnButtonPressed(object? sender, EventArgs e){
+            await Navigation.PushAsync(new GamePage());
+        }
+    }
+}
