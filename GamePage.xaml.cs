6c88260dbd54d0c5cbfe9afd8d33bda959575612
Anton Ostrovskiy <aoome@Antons-MacBook-Air.local>
2025-08-26 10:27:01 -0500

    Project Update

--- GamePage.xaml.cs
+++ GamePage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Media;
 
-// using Plugin.Maui.Audio;
+using Plugin.Maui.Audio;
 
 namespace Meowi
 {
@@ -12,9 +12,26 @@ namespace Meowi
             InitializeComponent();
         }
 
-        private void OnImageButtonClicked(object? sender, EventArgs e){
+        private void OnImageButtonClicked(object? sender, EventArgs e)
+        {
             Console.WriteLine("Meow Pressed");
             // TODO: Play cat noise
+            int[] numbers = { 0, 1, 2, 3 };
+        }
+
+        private void OnDeadClicked(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Dead Pressed");
+        }
+        private void OnAliveClicked(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Alive Pressed");
+        }
+
+        private int getRandIdx()
+        {
+            var rand = new Random();
+            return rand.Next() % 9;
         }
 
         // private SoundPlayer player = new SoundPlayer();
